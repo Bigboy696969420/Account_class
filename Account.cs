cab776c3b4781d9d29e755aff5e8543cbae7efaf
Bigboy696969420 <mc1101448@student.francistuttle.edu>
2026-02-10 14:00:35 -0600

    First commit for account class

---
+++ Account.cs
@@ -0,0 +1,68 @@
+namespace AccountClass;
+
+class Account
+{
+    public string AccountName { get; }
+    private double balance;
+    public double Balance
+    {
+        get => balance;
+        set
+        {
+            if (value >= 0) balance = value;
+        }
+    }
+
+    public Account(string AccountName, float balance)
+    {
+        this.AccountName = AccountName;
+        this.balance = balance;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine($"Owner: {AccountName} Banlance: ${balance}");
+    }
+
+    public void Deposit(float Money)
+    {
+        balance += Money;
+    }
+
+    public double GetBalance()
+    {
+        return balance;
+    }
+
+    public string GetAccountName()
+    {
+        return AccountName;
+    }
+
+    public bool withdraw(float Money)
+    {
+        if (balance < Money)
+        {
+            return false;
+        }
+        else
+        {
+            balance -= Money;
+            return true;
+        }
+    }
+
+    public bool transfer(Account accountTo, float Money)
+    {
+        if (balance < Money)
+        {
+            return false;
+        }
+        else
+        {
+            balance -= Money;
+            accountTo.Deposit(Money);
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System.IO.Compression;
+using System.Security.Cryptography.X509Certificates;
+using AccountClass;
+
+public class Program
+{
+    static void Main(string[] args)
+    {/*
+        Console.WriteLine("Testing Account1");
+        Account Account1 = new Account("Bob", 100);
+
+        Console.WriteLine("About to withdraw 150 from Account1");
+        if (Account1.withdraw(150))
+        {
+            Console.WriteLine("Success, new status: ");
+            Account1.Show();
+        }
+        else
+        {
+            Console.WriteLine("Insufficient funds, current account info: ");
+            Account1.Show();
+        }
+
+        Console.WriteLine("Depositing 200 into Account1, new status: ");
+        Account1.Deposit(200);
+        Account1.Show();
+
+        Console.WriteLine($"About to withdraw 150 from {Account1.AccountName}");
+        if (Account1.withdraw(150))
+        {
+            Console.WriteLine("Success, new status: ");
+            Account1.Show();
+        }
+        else
+        {
+            Console.WriteLine("Insufficient funds, current account info: ");
+            Account1.Show();
+        }
+
+        Console.WriteLine("Creating Account2 ");
+        Account Account2 = new Account("Mary", 100);
+        Account2.Show();
+
+        Console.WriteLine("Transferring 50 from Bob to Mary ");
+        if (Account1.transfer(Account2, 50))
+        {
+            Console.WriteLine("Success, new status: ");
+            Account1.Show();
+            Account2.Show();
+        }
+        else
+        {
+            Console.WriteLine("Insufficient funds, current account info: ");
+            Account1.Show();
+            Account2.Show();
+        }
+
+        Account1.Balance = 10000;
+        Account1.Show();
+*/
+        int? x1 = 20;
+        int? x2 = null;
+
+        nullable(x1);
+        nullable(x2);
+
+        string? s = null;
+        Account? a = null;
+    }
+    
+    public static void nullable(int? i)
+    {
+        if (i != null)
+        {
+            Console.WriteLine("The value is: " + i);
+        }
+        else 
+            Console.WriteLine("The value is null");
+    }
+    
+}
